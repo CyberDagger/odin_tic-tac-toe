fe79790227ad88036bb4627bea40624230286406
CyberDagger <cyberdagger003@gmail.com>
2025-06-02 15:49:51 +0100

    Player factory

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,13 @@ const Gameboard = (function () {
     return { setFieldSymbol, getFieldSymbol, getBoard };
 })();
 
+function createPlayer(name, symbol) {
+    return { name, symbol };
+}
+
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+
 /*----------------*/
 /* Test functions */
 /*----------------*/
